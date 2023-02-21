f77be71a1eb128d5953609de795977a189a0f8a9
Pau <pons.pau@gmail.com>
2023-02-21 15:53:24 +0100

    useTodos agregado

---
+++ useTodos/todoReducer.js
@@ -0,0 +1,33 @@
+
+export const todoReducer = ( initialState = [], action ) => {
+
+switch ( action.type ) {
+    case '[TODO] Add Todo':
+
+        return [...initialState, action.payload]
+        //throw new Error('action.type = ABC no esta implementada');    
+    
+    case '[TODO] Remove Todo':
+        return initialState.filter( todo => todo.id !== action.payload )
+
+    //siempre hay q devolver un state
+    case '[TODO] Toggle Todo':
+        return initialState.map( todo => {
+
+            if (todo.id === action.payload) {
+                return {
+                    ...todo,
+                    done: !todo.done
+
+                }
+
+            }
+
+            return todo;
+        });
+
+    default:
+        return initialState;
+}
+
+}
